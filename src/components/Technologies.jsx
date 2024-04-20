8007acbfc0014ae74fbba161919d83efc41209cc
Davalos77 <dennisavalos95@gmail.com>
2024-04-20 12:36:24 -0500

    changed technologies page

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -31,7 +31,7 @@ const Technologies = () => {
         transition={{ duration: 1.5 }}
         className="my-20 text-center text-4xl"
       >
-        Technologies
+        Skills
       </motion.h1>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
